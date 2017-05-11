9ae6c0ff236f042b4e9a327d669e417c2577b671
Domagoj Latečki <domagoj.latecki@gmail.com>
2017-05-11 12:38:07 +0200

    Added tests for generic type parameter hierarchy

--- src/test/java/at/doml/restinfo/TypeInformationTest.java
+++ src/test/java/at/doml/restinfo/TypeInformationTest.java
@@ -1,11 +1,15 @@
 package at.doml.restinfo;
 
 import org.junit.Test;
+import java.util.Collection;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.Set;
 import java.util.function.BiConsumer;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.fail;
 
 public class TypeInformationTest {
     
@@ -114,13 +118,198 @@ public class TypeInformationTest {
         });
     }
     
-    // TODO: add tests for generics
+    private interface TestTypeInformation {}
+    
+    @Test
+    public void typeWithSingleTypeParameterShouldHaveCorrectHierarchy() {
+        TypeInformation typeInformation = typeInformationFor(new TestTypeInformation() {
+            public List<String> test;
+        });
+        
+        assertTypeParameterHierarchy(
+                typeInformationFor(
+                        "java.util.List",
+                        typeInformationFor("java.lang.String")
+                ),
+                typeInformation
+        );
+    }
+    
+    @Test
+    public void typeWithSingleNestedTypeParameterShouldHaveCorrectHierarchy() {
+        TypeInformation typeInformation = typeInformationFor(new TestTypeInformation() {
+            public List<Set<Collection<String>>> test;
+        });
+        
+        assertTypeParameterHierarchy(
+                typeInformationFor(
+                        "java.util.List",
+                        typeInformationFor(
+                                "java.util.Set",
+                                typeInformationFor(
+                                        "java.util.Collection",
+                                        typeInformationFor("java.lang.String")
+                                )
+                        )
+                ),
+                typeInformation
+        );
+    }
+    
+    @Test
+    public void typeWithMultipleTypeParametersShouldHaveCorrectHierarchy() {
+        TypeInformation typeInformation = typeInformationFor(new TestTypeInformation() {
+            public Map<String, Integer> test;
+        });
+        
+        assertTypeParameterHierarchy(
+                typeInformationFor(
+                        "java.util.Map",
+                        typeInformationFor("java.lang.String"),
+                        typeInformationFor("java.lang.Integer")
+                ),
+                typeInformation
+        );
+    }
+    
+    @Test
+    public void typeWithMultipleNestedTypeParametersShouldHaveCorrectHierarchy() {
+        TypeInformation typeInformation = typeInformationFor(new TestTypeInformation() {
+            public Map<Long, Map<String, Map<Map<Character, Short>, List<Boolean>>>> test;
+        });
+        
+        assertTypeParameterHierarchy(
+                typeInformationFor(
+                        "java.util.Map",
+                        typeInformationFor("java.lang.Long"),
+                        typeInformationFor(
+                                "java.util.Map",
+                                typeInformationFor("java.lang.String"),
+                                typeInformationFor(
+                                        "java.util.Map",
+                                        typeInformationFor(
+                                                "java.util.Map",
+                                                typeInformationFor("java.lang.Character"),
+                                                typeInformationFor("java.lang.Short")
+                                        ),
+                                        typeInformationFor(
+                                                "java.util.List",
+                                                typeInformationFor("java.lang.Boolean")
+                                        )
+                                )
+                        )
+                ),
+                typeInformation
+        );
+    }
+    
+    @Test
+    public void arrayTypesWithSingleTypeParameterShouldHaveCorrectHierarchy() {
+        TypeInformation typeInformation = typeInformationFor(new TestTypeInformation() {
+            public List<String>[] test;
+        });
+        
+        assertTypeParameterHierarchy(
+                typeInformationFor(
+                        "java.util.List", 1,
+                        typeInformationFor("java.lang.String")
+                ),
+                typeInformation
+        );
+        
+        typeInformation = typeInformationFor(new TestTypeInformation() {
+            public Set<Integer>[][][] test;
+        });
+        
+        assertTypeParameterHierarchy(
+                typeInformationFor(
+                        "java.util.Set", 3,
+                        typeInformationFor("java.lang.Integer")
+                ),
+                typeInformation
+        );
+    }
+    
+    @Test
+    public void typesWithSingleArrayTypeParameterShouldHaveCorrectHierarchy() {
+        TypeInformation typeInformation = typeInformationFor(new TestTypeInformation() {
+            public List<String[]> test;
+        });
+        
+        assertTypeParameterHierarchy(
+                typeInformationFor(
+                        "java.util.List",
+                        typeInformationFor("java.lang.String", 1)
+                ),
+                typeInformation
+        );
+        
+        typeInformation = typeInformationFor(new TestTypeInformation() {
+            public Set<Integer[][][]> test;
+        });
+        
+        assertTypeParameterHierarchy(
+                typeInformationFor(
+                        "java.util.Set",
+                        typeInformationFor("java.lang.Integer", 3)
+                ),
+                typeInformation
+        );
+    }
+    
+    @Test
+    public void arrayTypesWithMultipleNestedArrayTypeParametersShouldHaveCorrectHierarchy() {
+        TypeInformation typeInformation = typeInformationFor(new TestTypeInformation() {
+            public Map<Long[], Map<Map<Character[], Short>[][][], boolean[][]>>[][][][][] test;
+        });
+        
+        assertTypeParameterHierarchy(
+                typeInformationFor(
+                        "java.util.Map", 5,
+                        typeInformationFor("java.lang.Long", 1),
+                        typeInformationFor(
+                                "java.util.Map",
+                                typeInformationFor(
+                                        "java.util.Map", 3,
+                                        typeInformationFor("java.lang.Character", 1),
+                                        typeInformationFor("java.lang.Short")
+                                ),
+                                typeInformationFor("boolean", 2)
+                        )
+                ),
+                typeInformation
+        );
+    }
     
     //
     // HELPER METHODS
     //
     private static TypeInformation typeInformationFor(Class<?> clazz) {
         return new TypeInformation(clazz.getTypeName());
+    }
+    
+    private static TypeInformation typeInformationFor(String type, TypeInformation... typeParameters) {
+        return typeInformationFor(type, 0, typeParameters);
+    }
+    
+    private static TypeInformation typeInformationFor(String type, int arrayDimension,
+                                                      TypeInformation... typeParameters) {
+        return new TypeInformation(type, typeParameters, arrayDimension);
+    }
+    
+    private static TypeInformation typeInformationFor(TestTypeInformation testTypeInformation) {
+        try {
+            return new TypeInformation(
+                    testTypeInformation.getClass()
+                            .getField("test")
+                            .getGenericType()
+                            .getTypeName()
+            );
+        } catch (NoSuchFieldException ignored) {
+            fail("provided object has no field named \"test\" from which to fetch type information");
+        }
+        
+        return null;
     }
     
     private void testTypes(BiConsumer<String, TypeInformation> assertions) {
@@ -134,6 +323,21 @@ public class TypeInformationTest {
     //
     private static void assertType(String expectedTypeString, TypeInformation ti) {
         assertEquals("type is incorrect", expectedTypeString, ti.getType());
+    }
+    
+    private static void assertTypeParameterHierarchy(TypeInformation expectedHierarchy,
+                                                     TypeInformation actualHierarchy) {
+        TypeInformation[] actualTypeParameters = actualHierarchy.getTypeParameters();
+        TypeInformation[] expectedTypeParameters = expectedHierarchy.getTypeParameters();
+        
+        assertType(expectedHierarchy.getType(), actualHierarchy);
+        assertEquals("invalid number of type paramaters for " + actualHierarchy.getType(),
+                expectedTypeParameters.length, actualTypeParameters.length);
+        assertThatTypeIsArrayWithDimension(expectedHierarchy.getArrayDimension(), actualHierarchy);
+        
+        for (int i = 0; i < expectedTypeParameters.length; i++) {
+            assertTypeParameterHierarchy(expectedTypeParameters[i], actualTypeParameters[i]);
+        }
     }
     
     private static void assertThatTypeIsArrayWithDimension(int expectedDimension, TypeInformation ti) {
